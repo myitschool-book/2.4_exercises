014ef98c9a7dd805b381ca380f3487c8dae84970
d.yacenko <d.yacenko@gmail.com>
2020-01-03 12:03:41 +0300

    homework add

---
+++ HomeWork241/app/src/main/java/ru/samsung/itschool/book/MainActivity.java
@@ -0,0 +1,19 @@
+package ru.samsung.itschool.book;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
---
+++ HomeWork241/app/src/test/java/ru/samsung/itschool/book/ExampleUnitTest.java
@@ -0,0 +1,129 @@
+package ru.samsung.itschool.book;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+
+import java.util.Random;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(RobolectricTestRunner.class)
+public class ExampleUnitTest {
+    public static final String ZERO = "0";
+    public static final String NEG_ONE_ = "-1.0";
+    public static final String ZERO_ = "0.0";
+    public static final String ONE_ = "1.0";
+    public static final String TWO_ = "2.0";
+    public static final String ANY = "any";
+    public static final String NONE = "none";
+    private MainActivity activity;
+    static StringBuffer sb;
+    static int salt;
+
+    @BeforeClass
+    public static void report(){
+        salt=new Random(System.currentTimeMillis()).nextInt(999);
+        sb=new StringBuffer();
+        sb.append(String.format("%03d",salt));
+    }
+
+    @Before
+    public void setup() {
+        activity = Robolectric.buildActivity(MainActivity.class)
+                .create().get();
+    }
+
+    @Test
+    public void testButton1() throws Exception {
+        try {
+            MainActivity activity = Robolectric.buildActivity(MainActivity.class)
+                    .create().get();
+            Button view = (Button) activity.findViewById(R.id.run);
+            Assert.assertNotNull(view);
+            EditText et_a = (EditText) activity.findViewById(R.id.a);
+            Assert.assertNotNull(et_a);
+            EditText et_b = (EditText) activity.findViewById(R.id.b);
+            Assert.assertNotNull(et_b);
+            EditText et_c = (EditText) activity.findViewById(R.id.c);
+            Assert.assertNotNull(et_c);
+            TextView tv = (TextView) activity.findViewById(R.id.res);
+            Assert.assertNotNull(tv);
+            et_a.setText(ZERO);
+            et_b.setText(ZERO);
+            et_c.setText(ZERO);
+            view.performClick();
+            org.junit.Assert.assertEquals(tv.getText().toString(), ANY);
+            sb.append(",OK");
+        } catch (Throwable t){}
+    }
+
+    @Test
+    public void testButton2() throws Exception {
+        try {
+            MainActivity activity = Robolectric.buildActivity(MainActivity.class)
+                    .create().get();
+            Button view = (Button) activity.findViewById(R.id.run);
+            Assert.assertNotNull(view);
+            EditText et_a = (EditText) activity.findViewById(R.id.a);
+            Assert.assertNotNull(et_a);
+            EditText et_b = (EditText) activity.findViewById(R.id.b);
+            Assert.assertNotNull(et_b);
+            EditText et_c = (EditText) activity.findViewById(R.id.c);
+            Assert.assertNotNull(et_c);
+            TextView tv = (TextView) activity.findViewById(R.id.res);
+            Assert.assertNotNull(tv);
+            et_a.setText(NEG_ONE_);
+            et_b.setText(TWO_);
+            et_c.setText(ZERO);
+            view.performClick();
+            org.junit.Assert.assertEquals(tv.getText().toString(), TWO_ + " " + ZERO_);
+            sb.append(",OK");
+        } catch (Throwable t){}
+    }
+
+    @Test
+    public void testButton3() throws Exception {
+        try {
+            MainActivity activity = Robolectric.buildActivity(MainActivity.class)
+                    .create().get();
+            Button view = (Button) activity.findViewById(R.id.run);
+            Assert.assertNotNull(view);
+            EditText et_a = (EditText) activity.findViewById(R.id.a);
+            Assert.assertNotNull(et_a);
+            EditText et_b = (EditText) activity.findViewById(R.id.b);
+            Assert.assertNotNull(et_b);
+            EditText et_c = (EditText) activity.findViewById(R.id.c);
+            Assert.assertNotNull(et_c);
+            TextView tv = (TextView) activity.findViewById(R.id.res);
+            Assert.assertNotNull(tv);
+            et_a.setText(ONE_);
+            et_b.setText(TWO_);
+            et_c.setText(TWO_);
+            view.performClick();
+            org.junit.Assert.assertEquals(tv.getText().toString(), NONE);
+            sb.append(",OK");
+        }catch(Throwable t){}
+    }
+
+    @AfterClass
+    public static void printResult(){
+        System.err.println("\n\n=============================\nВАШ РЕЗУЛЬТАТ: "+sb.toString().hashCode()+""+salt+"\n=============================\n");
+
+    }
+
+}
